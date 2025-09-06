e1dd777453e2d7ee0c4dc313d9165d7733087bb2
AnaMargaridaSilva <161249955+AnaMargaridaSilva@users.noreply.github.com>
2025-09-06 10:35:32 +0100

    Create openai_span_detection_2.py

---
+++ template/openai_span_detection_2.py
@@ -0,0 +1,7 @@
+{
+    "task_description": "Please perform a causal span detection according to the following rules\n1. Please insert <c> before the word where the cause span begins and </c> after the word where the span ends.\n2. Please insert <e> before the word where the effect span begins and </e> after the word where the span ends.\n3. If there are multiple pairs of cause and effect, use the tags :",
+    "header_example": "Annotation Examples:\n",
+    "format_text": "Text: {}\n",
+    "format_class": "Text with tags: \n{}\n",
+    "question": "According to the policy above, please add the appropriate tags in the following text.\n"
+}
