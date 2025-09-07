3a68bf537a266a8a9dfc08e2e7c4eb1155a592c7
AnaMargaridaSilva <161249955+AnaMargaridaSilva@users.noreply.github.com>
2025-09-07 17:22:39 +0100

    Update __init__.py

--- src/prediction/__init__.py
+++ src/prediction/__init__.py
@@ -1,2 +1,3 @@
 from .hf_encoder import predict as predict_hf_encoder
-from .openai import predict as predict_openai
+#from .openai import predict as predict_openai
+from .gemini import predict as predict_gemini
